14cff30e0f0f2740d49986294274908c9ffe7dfa
thrilok209 <thrilok2000@gmail.com>
2016-10-26 11:49:13 +0530

    game completed

--- app2.js
+++ app2.js
@@ -5,17 +5,17 @@ function myMove() {
 var x =  event.key;
   var elem = document.getElementById("car");
 
-  if(x=="a" && pos1>=0 && count==0){
+  if((x=="a" || x=="A") && pos1>=0 && count==0){
   pos1 = pos1-10;}
-if(x=="d" && pos1<=750 && count==0){
+if((x=="d" || x=="D")&& pos1<=750 && count==0){
   pos1 = pos1+10 ;}
-if(x=="w" && pos>=0 && count==0){
+if((x=="W" || x=="w") && pos>=0 && count==0){
   pos = pos-10;}
-if(x=="s" && pos<=350 && count==0){
+if((x=="S" || x=="s") && pos<=350 && count==0){
   pos = pos+10;}
 
   if((pos==0 || pos==350 ||pos1==0 || pos1==750) && count==0){
-  alert("game Over");
+  alert(" GAME OVER !! Better Luck Next Time");
   count++;
   }
 
